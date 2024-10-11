3f15160f0c2f41f75365c972220662184a2ee7e0
Nirban Chakraborty <nirban256@gmail.com>
2024-10-11 11:46:32 +0530

    first commit

---
+++ components/user-menu.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from 'react';
+import { UserButton } from '@clerk/nextjs';
+import { ChartNoAxesGantt } from 'lucide-react';
+
+const UserMenu = () => {
+    return (
+        <UserButton appearance={{
+            elements: {
+                avatarBox: "w-10 h-10"
+            }
+        }}>
+            <UserButton.MenuItems>
+                <UserButton.Link label='My events' labelIcon={<ChartNoAxesGantt size={15} />} href='/events' />
+                <UserButton.Action label='manageAccount' />
+            </UserButton.MenuItems>
+        </UserButton>
+    )
+}
+
+export default UserMenu;
